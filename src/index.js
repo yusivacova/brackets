634013e76c052668ebee3a1361ad2821dd26bf96
yusivacova <sivacovayulia@gmail.com>
2023-01-26 01:39:48 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let copyStr = str;
+    let i = 0;
+
+
+    function cleansDouble (){
+        bracketsConfig.forEach(function(item){
+            copyStr = copyStr.replace(`${item[0]}${item[1]}`, '');
+        }
+        )};
+        
+        do{
+            cleansDouble ();
+            i++;
+        } while (i < 50);
+       
+        let result = (copyStr.length === 0) ? true : false;
+        
+        return result;
 }
+
+
+
